a8fb1b7afed3ede9cceeb048ba1475dfb6e01d0e
Remy Sharp <remy@remysharp.com>
2016-03-18 17:07:39 +0000

    Adds a tiny Snyk vuln integration
    This mimics the github code fairly closely: has a (working link) placeholder to check for vulns for the specific package, then if JS is available, it'll make an XHR request to Snyk's API and update with any vulns in the package.
    
    The link has a number of states:
    
    - [Test for vulnerabilities] on Snyk (non JS state)
    - [No known vulnerabilities] found on Snyk
    - [1 vulnerable dependency] found on Snyk (package has vulns in dependencies)
    - [4 vulnerable dependencies] found on Snyk
    - [1 known vulnerability] found on Snyk (package *is* vulnerable)
    - [5 known vulnerabilities] found on Snyk
    
    This change also updates the csp to include snyk.io's specific npm API endpoint in the `connectSrc` list.

---
+++ assets/scripts/snyk.js
@@ -0,0 +1,76 @@
+var snyk = module.exports = function () {
+  $(snyk.init);
+  return snyk;
+};
+
+snyk.api = 'https://snyk.io/api/v1/';
+
+snyk.init = function() {
+  snyk.element = $('#snyk_vulns');
+
+  // check that the element includes everything we need to perform the XHR
+  // request for vulnerabilities
+  if (!snyk.element.length) {
+    return;
+  }
+
+  var data = snyk.element.data();
+
+  if (!data.name) {
+    return;
+  }
+
+  // kick off XHR requests
+  snyk.getVulnerabilities(data.name);
+};
+
+snyk.getVulnerabilities = function (pkg) {
+  var root = snyk.api + 'vuln/npm';
+
+  return $.getJSON(root + '/' + pkg).done(function (res) {
+    var vulns = {
+      vulns: 0,
+      directVulns: 0,
+      depVulns: 0
+    };
+
+    if (!res.ok) {
+      // I wanted to use .reduce here, but wasn't sure about target support
+      // and couldn't find any other use of reduce in the asset/scripts dir
+      // so using .forEach (also appears in star.js).
+      res.vulnerabilities.forEach(function (curr) {
+        vulns.vulns++;
+
+        if (curr.from.length === 1) {
+          vulns.directVulns++;
+        } else {
+          vulns.depVulns++;
+        }
+
+        return vulns;
+      });
+    }
+
+    var label = vulns.depVulns + ' vulnerable ' + pl('dependency', vulns.depVulns);
+    if (vulns.vulns === 0) {
+      label = 'No known vulnerabilities';
+    } else if (vulns.directVulns) {
+      label = vulns.directVulns + ' known ' + pl('vulnerability', vulns.directVulns);
+    }
+
+    // double check in case we've been called directly
+    if (snyk.element.length) {
+      snyk.element.find('span').show();
+      snyk.element.find('a').text(label);
+    } else {
+      return label;
+    }
+  });
+};
+
+function pl(word, count) {
+  if (count > 1) {
+    return word.slice(0, -1) + 'ies';
+  }
+  return word;
+}
